0544b9d6017344eae729186925b6079ff268e65e
seonoopy <seonoopy94@gmail.com>
2024-10-01 22:28:01 +0900

    tailwindcss

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,4 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+// styles
+
+// import "@fortawesome/fontawesome-free/css/all.min.css";
+import "@/assets/styles/tailwind.css";
+
 createApp(App).mount('#app')
